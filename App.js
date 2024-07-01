b18f693af8c548735afed11ca5fa874ee24cf886
rizqialfani <rizqialfani01@gmail.com>
2024-07-01 21:09:45 +0700

    Create WeatherApp UI

--- App.js
+++ App.js
@@ -1,52 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { View, FlatList, StyleSheet, Text } from 'react-native'
-
-const Item = ({ title }) => (
-  <View style={styles.item}>
-    <Text style={styles.title}>{title}</Text>
-  </View>
-)
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import WeatherSearch from './src/components/weatherSearch'
+import WeatherInfo from './src/components/weatherInfo'
 
 const App = () => {
-  const [data, setData] = useState([])
-  const renderItem = ({ item }) => <Item title={item.title} />
-
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        setData(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  })
-
   return (
     <View style={styles.container}>
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
+      <WeatherSearch />
+      <WeatherInfo />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-  },
-  item: {
-    backgroundColor: 'grey',
     padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  title: {
-    color: 'white',
-    fontSize: 32,
   },
 })
 
